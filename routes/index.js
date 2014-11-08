f460a29f9b18ac03eb61b692c2e832eee06985ae
Chelsea <chelsea.bailey@students.olin.edu>
2014-11-08 11:16:24 -0500

    mobile app!

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {title: 'Welcome to Rice Checker', state: 2});
 });
 
 module.exports = router;
